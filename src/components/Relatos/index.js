8cd0f7adbfcccd326b45b30aacebbf1dc3ce2828
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-14 22:57:33 -0300

    Carragar Relatos

--- src/components/Relatos/index.js
+++ src/components/Relatos/index.js
@@ -1,29 +1,42 @@
-import { useEffect } from "react";
-import { getRelatos } from "../../services/relatos.js"
+import { useEffect, useState } from "react";
+import { getRelatos } from "../../services/relatos.js";
 import "./Relatos.css";
 
 const Relatos = () => {
+  const [listaRelatos, setListaRelatos] = useState([]);
 
   useEffect(() => {
-    const listaRelatos = getRelatos()
-}, [])
+    fetchRelatos();
+  }, []);
+
+  const fetchRelatos = async () => {
+    setListaRelatos(await getRelatos());
+  };
 
   return (
     <section>
       <h2>Relatos</h2>
       <div className="relatos">
-        {/* map */}
-        <div className="relato">
-          <div className="data">
-            <div>
-              <h1>Dia</h1>
-              <p>Mês</p>
-            </div>
-            <div className="barra" />
-          </div>
+        {listaRelatos.map((relato) => {
+          const data = new Date(relato.data);
+          const dia = String(data.getDate()).padStart(2, "0");
+          const mes = data.toLocaleString("pt-BR", { month: "long" });
+          const previa = relato.texto.slice(0, 200);
 
-          <p className="previa">Prévia texto</p>
-        </div>
+          return (
+            <div key={relato.id} className="relato">
+              <div className="data">
+                <div>
+                  <h1>{dia}</h1>
+                  <p>{mes}</p>
+                </div>
+                <div className="barra" />
+              </div>
+
+              <p className="previa">{previa}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
