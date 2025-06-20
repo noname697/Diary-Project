570a1789a2d0810dee581fc81b540ce748797066
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-06-20 15:31:05 -0300

    ODEIO A VERCEL!!!!!

--- API/app.js
+++ API/app.js
@@ -3,7 +3,7 @@ const Controller = require("./controllers/controller.js");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8000;;
+const PORT = 8000;
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
@@ -13,6 +13,7 @@ app.get("/", (req, res) => Controller.pegaTodos(req, res));
 app.get("/:id", (req, res) => Controller.pegaUm(req, res));
 app.post("/", (req, res) => Controller.postaRelato(req, res));
 app.put("/:id", (req, res) => Controller.editaRelato(req, res));
+app.put("/favorito/:id", (req, res) => Controller.mudaFavorito(req, res));
 app.put("/favorito/:id", (req, res) => Controller.mudaFavorito(req, res));
 app.delete("/:id", (req, res) => Controller.deletaRelato(req, res));
 
--- src/services/relatos.js
+++ src/services/relatos.js
@@ -1,44 +1,46 @@
 import axios from "axios";
 
-async function getRelatos() {
+const relatosAPI = axios.create({ baseURL: "http://localhost:8000/" });
 
-  const response = await axios.get("/api/");
+async function getRelatos() {
+  const response = await relatosAPI.get("/");
+
   return response.data;
 }
 
 async function postRelatos(dados) {
+  const response = await axios.post("http://localhost:8000/", dados);
 
-  const response = await axios.post("/api/", dados);
   console.log(response.data);
 }
 
 async function getRelatoEspecifico(id) {
+  const response = await axios.get(`http://localhost:8000/${id}`);
 
-  const response = await axios.get(`/api/${id}`);
   return response.data;
 }
 
 async function putRelato(id, dados) {
+  const response = await axios.put(`http://localhost:8000/${id}`, dados);
 
-  const response = await axios.put(`/api/${id}`, dados);
   console.log(response.data);
 }
 
 async function deleteRelato(id) {
+  const response = await axios.delete(`http://localhost:8000/${id}`);
 
-  const response = await axios.delete(`/api/${id}`);
   console.log(response.data);
 }
 
 async function postFavorito(id) {
+  const response = await axios.put(`http://localhost:8000/favorito/${id}`);
 
-  const response = await axios.put(`/api/favorito/${id}`);
   console.log(response.data);
 }
 
 async function getFavoritos() {
+  const response = await axios.get("http://localhost:8000/favoritos");
 
-  const response = await axios.get("/api/favoritos");
   return response.data;
 }
 
