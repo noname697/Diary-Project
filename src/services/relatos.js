9e02f832d88186d01caa90728842499c8e580dbc
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-29 11:26:51 -0300

    Atualizando as rotas

--- src/services/relatos.js
+++ src/services/relatos.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const relatosAPI = axios.create({ baseURL: "https://diary-api-fbaj.onrender.com//" });
+const relatosAPI = axios.create({ baseURL: "https://diary-api-fbaj.onrender.com/" });
 
 async function getRelatos() {
   const response = await relatosAPI.get("/");
@@ -9,37 +9,37 @@ async function getRelatos() {
 }
 
 async function postRelatos(dados) {
-  const response = await axios.post("https://diary-api-fbaj.onrender.com//", dados);
+  const response = await axios.post("https://diary-api-fbaj.onrender.com/", dados);
 
   console.log(response.data);
 }
 
 async function getRelatoEspecifico(id) {
-  const response = await axios.get(`https://diary-api-fbaj.onrender.com//${id}`);
+  const response = await axios.get(`https://diary-api-fbaj.onrender.com/${id}`);
 
   return response.data;
 }
 
 async function putRelato(id, dados) {
-  const response = await axios.put(`https://diary-api-fbaj.onrender.com//${id}`, dados);
+  const response = await axios.put(`https://diary-api-fbaj.onrender.com/${id}`, dados);
 
   console.log(response.data);
 }
 
 async function deleteRelato(id) {
-  const response = await axios.delete(`https://diary-api-fbaj.onrender.com//${id}`);
+  const response = await axios.delete(`https://diary-api-fbaj.onrender.com/${id}`);
 
   console.log(response.data);
 }
 
 async function postFavorito(id) {
-  const response = await axios.put(`https://diary-api-fbaj.onrender.com//favorito/${id}`);
+  const response = await axios.put(`https://diary-api-fbaj.onrender.com/favorito/${id}`);
 
   console.log(response.data);
 }
 
 async function getFavoritos() {
-  const response = await axios.get("https://diary-api-fbaj.onrender.com//favoritos");
+  const response = await axios.get("https://diary-api-fbaj.onrender.com/favoritos");
 
   return response.data;
 }
